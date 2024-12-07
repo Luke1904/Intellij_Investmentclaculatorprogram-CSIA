15c45485735573c74af309f8f8df1a0080bbf1b6
Luke1904 <luca.petruti2@gmail.com>
2024-12-07 14:55:11 +0200

    commit

--- src/FolderManager.java
+++ src/FolderManager.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class FolderManager {
     public static void createSaveFolder() {
-        String directoryPath = "X:\\Applications\\IntelliJ IDEA Community Edition 2023.3.5\\bin\\Saves";
+        String directoryPath = "X:\\Saves";
         File folder = new File(directoryPath);
         if (!folder.exists()) folder.mkdirs();
     }
